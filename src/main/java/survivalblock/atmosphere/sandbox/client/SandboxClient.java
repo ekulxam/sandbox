c7e4af55d404e9f0ffca00f7ecc10a16a338880d
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-01-19 01:52:01 -0800

    aaaa2

--- src/main/java/survivalblock/atmosphere/sandbox/client/SandboxClient.java
+++ src/main/java/survivalblock/atmosphere/sandbox/client/SandboxClient.java
@@ -11,6 +11,7 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.util.FastColor;
 import net.minecraft.util.Mth;
 import org.joml.Matrix4f;
 
@@ -24,22 +25,19 @@ public class SandboxClient implements ClientModInitializer {
             Tesselator tessellator = Tesselator.getInstance();
             PoseStack matrixStack = drawContext.pose();
             Matrix4f matrix4f = matrixStack.last().pose();
-            BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION);
-            bufferBuilder.addVertex(matrix4f, center.x, center.y, 5);
-            bufferBuilder.addVertex(matrix4f, previous.x, previous.y, 5);
-            float angle = 72 * Mth.DEG_TO_RAD;
-            bufferBuilder.addVertex(matrix4f, (int) (Mth.cos(angle) * radius), (int) (Mth.sin(angle) * radius), 5);
-            RenderSystem.setShader(GameRenderer::getPositionColorShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 
-            BufferUploader.drawWithShader(bufferBuilder.build());
+            drawContext.fill(center.x - 1, center.y - 1, center.x + 1, center.y + 1, 0xFF0000FF);
+            drawContext.fill(previous.x - 1, previous.y - 1, previous.x + 1, previous.y + 1, 0xFFFF0000);
+            int max = 5;
 
-            /*
-            int max = 2;
             for (int i = 1; i < max; i++) {
                 float angle = (360F / max) * i * Mth.DEG_TO_RAD;
                 Coordinate updated = center.add((int) (Mth.cos(angle) * radius), (int) (Mth.sin(angle) * radius));
 
+                BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.Mode.TRIANGLE_STRIP, DefaultVertexFormat.POSITION);
+
+                bufferBuilder.addVertex(matrix4f, center.x, center.y, 5);
+                bufferBuilder.addVertex(matrix4f, previous.x, previous.y, 5);
                 bufferBuilder.addVertex(matrix4f, updated.x, updated.y, 5);
 
                 RenderSystem.setShader(GameRenderer::getPositionColorShader);
@@ -48,13 +46,14 @@ public class SandboxClient implements ClientModInitializer {
                 drawContext.fill(center.x - 1, center.y - 1, center.x + 1, center.y + 1, 0xFF0000FF);
                 drawContext.fill(previous.x - 1, previous.y - 1, previous.x + 1, previous.y + 1, 0xFFFF0000);
                 drawContext.fill(updated.x - 1, updated.y - 1, updated.x + 1, updated.y + 1, 0xFFFF0000);
+
+                MeshData built = bufferBuilder.build();
+                BufferUploader.drawWithShader(built);
+                built.close();
+
+                drawContext.fill(updated.x - 1, updated.y - 1, updated.x + 1, updated.y + 1, FastColor.ARGB32.lerp((float) i / max, 0xFFFF0000, 0xFF00FF00));
                 previous = updated;
             }
-
-            MeshData built = bufferBuilder.build();
-            BufferUploader.drawWithShader(built);
-
-             */
         });
     }
 
