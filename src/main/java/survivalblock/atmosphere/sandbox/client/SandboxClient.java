b7477b3b7ddd8019dba51724297489b304d7782f
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-01-19 01:54:56 -0800

    a3

--- src/main/java/survivalblock/atmosphere/sandbox/client/SandboxClient.java
+++ src/main/java/survivalblock/atmosphere/sandbox/client/SandboxClient.java
@@ -4,12 +4,12 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.BufferUploader;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.MeshData;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.util.FastColor;
 import net.minecraft.util.Mth;
@@ -19,26 +19,31 @@ public class SandboxClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register((drawContext, tickCounter) -> {
+            if (!Screen.hasAltDown()) {
+                return;
+            }
             Coordinate center = new Coordinate(drawContext.guiWidth() / 2, drawContext.guiHeight() / 2);
             int radius = 20;
             Coordinate previous = center.add(radius, 0);
-            Tesselator tessellator = Tesselator.getInstance();
             PoseStack matrixStack = drawContext.pose();
-            Matrix4f matrix4f = matrixStack.last().pose();
 
             drawContext.fill(center.x - 1, center.y - 1, center.x + 1, center.y + 1, 0xFF0000FF);
             drawContext.fill(previous.x - 1, previous.y - 1, previous.x + 1, previous.y + 1, 0xFFFF0000);
-            int max = 5;
+            int max = 6;
 
-            for (int i = 1; i < max; i++) {
+            for (int i = 1; i < max + 1; i++) {
                 float angle = (360F / max) * i * Mth.DEG_TO_RAD;
                 Coordinate updated = center.add((int) (Mth.cos(angle) * radius), (int) (Mth.sin(angle) * radius));
 
-                BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.Mode.TRIANGLE_STRIP, DefaultVertexFormat.POSITION);
+                drawContext.fill(updated.x - 1, updated.y - 1, updated.x + 1, updated.y + 1, FastColor.ARGB32.lerp((float) i / max, 0xFFFF0000, 0xFF00FF00));
 
-                bufferBuilder.addVertex(matrix4f, center.x, center.y, 5);
-                bufferBuilder.addVertex(matrix4f, previous.x, previous.y, 5);
-                bufferBuilder.addVertex(matrix4f, updated.x, updated.y, 5);
+                BufferBuilder bufferBuilder = Tesselator.getInstance().begin(VertexFormat.Mode.TRIANGLE_STRIP, DefaultVertexFormat.POSITION);
+
+                Matrix4f matrix4f = matrixStack.last().pose();
+                RenderSystem.disableCull();
+                bufferBuilder.addVertex(matrix4f, previous.x, previous.y, 5).setColor(0xFF000000);
+                bufferBuilder.addVertex(matrix4f, updated.x, updated.y, 5).setColor(0xFF000000);
+                bufferBuilder.addVertex(matrix4f, center.x, center.y, 5).setColor(0xFF000000);
 
                 RenderSystem.setShader(GameRenderer::getPositionColorShader);
                 RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
@@ -47,11 +52,8 @@ public class SandboxClient implements ClientModInitializer {
                 drawContext.fill(previous.x - 1, previous.y - 1, previous.x + 1, previous.y + 1, 0xFFFF0000);
                 drawContext.fill(updated.x - 1, updated.y - 1, updated.x + 1, updated.y + 1, 0xFFFF0000);
 
-                MeshData built = bufferBuilder.build();
-                BufferUploader.drawWithShader(built);
-                built.close();
+                BufferUploader.drawWithShader(bufferBuilder.build());
 
-                drawContext.fill(updated.x - 1, updated.y - 1, updated.x + 1, updated.y + 1, FastColor.ARGB32.lerp((float) i / max, 0xFFFF0000, 0xFF00FF00));
                 previous = updated;
             }
         });
