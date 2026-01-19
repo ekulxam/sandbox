06b2ea15f867b31046d602501582b3bd645ebbfe
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-01-19 01:38:12 -0800

    Here we go again

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sandbox
+
+Minecraft modding playground
---
+++ src/main/java/survivalblock/atmosphere/sandbox/datagen/SandboxDataGenerator.java
@@ -0,0 +1,10 @@
+package survivalblock.atmosphere.sandbox.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SandboxDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+	}
+}
